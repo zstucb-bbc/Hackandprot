a9ba8f5b05de100e2a4827f0e7561dc05c414e8c
zstucb-bbc <bestucki31@gmail.com>
2019-11-08 13:01:05 +0100

    Adding Login

--- HackingandSavingPage/Areas/SqlInjection/Controllers/SqlInjectionController.cs
+++ HackingandSavingPage/Areas/SqlInjection/Controllers/SqlInjectionController.cs
@@ -10,11 +10,15 @@ namespace HackingandSavingPage.Areas.SqlInjectionController.Controllers
     public class SqlInjectionController : Controller
     {
         public IActionResult Index()
-        {
+            {
             SqlInjectionModel model = new SqlInjectionModel();
             return View(model);
         }
 
+        public ActionResult Login(LoginModel model)
+        {
+            return View("Login", model);
+        }
 
         public ActionResult Save(SqlInjectionModel model)
         {
---
+++ HackingandSavingPage/Areas/SqlInjection/Models/LoginModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackingandSavingPage.Areas.SqlInjection.Models
+{
+    public class LoginModel
+    {
+        [DisplayName("User Name")]
+        [Required]
+        public String UserName { get; 
+            set; }
+
+      
+
+        [DisplayName("Password")]
+        [Required]
+        public String Password
+        {
+            get;
+            set;
+        }
+           
+    }
+}
