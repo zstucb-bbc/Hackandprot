05da484079e51ae1dc4c61431535ca3d2ff58c07
zstucb-bbc <bestucki31@gmail.com>
2019-11-10 18:45:28 +0100

    Adding logger

--- HackingandSavingPage/Areas/SqlInjection/Controllers/SqlInjectionController.cs
+++ HackingandSavingPage/Areas/SqlInjection/Controllers/SqlInjectionController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HackingandSavingPage.Areas.SqlInjection.Models;
+using HackingandSavingPage.Features;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HackingandSavingPage.Areas.SqlInjectionController.Controllers
 {
     public class SqlInjectionController : Controller
     {
+
+        Logger logger = new Logger();
         public IActionResult Index()
             {
             SqlInjectionModel model = new SqlInjectionModel();
@@ -17,6 +20,7 @@ namespace HackingandSavingPage.Areas.SqlInjectionController.Controllers
 
         public ActionResult Login(LoginModel model)
         {
+            logger.log("User trying to login with, " + model.UserName + "as Username");
             return View("Login", model);
         }
 
---
+++ HackingandSavingPage/Features/Logger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackingandSavingPage.Features
+{
+    public class Logger
+    {
+
+    
+    public void log(String lines)
+    {
+
+        // Write the string to a file.append mode is enabled so that the log
+        // lines get appended to  test.txt than wiping content and writing the log
+
+        System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\log.txt", true);
+        file.WriteLine(lines);
+
+        file.Close();
+
+    }
+
+    }
+}
